d53d4132650ad6e55857e996c4e1e37c427a2a01
Kofsterx22 <nuku.monu98@gmail.com>
2020-10-26 14:18:05 -0400

    commiting merge from master

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -17,6 +17,10 @@ function sortFunction(a, b, key) {
   return 0;
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
@@ -29,8 +33,31 @@ document.body.addEventListener('submit', async (e) => {
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
-      console.log('fromServer', fromServer);
-    })
+      
+if (document.querySelector('.flex-inner')){
+document.querySelector('.flex-inner').remove();
+}      
+const newArr =range(10);
+const newArr2 = newArr.map(()=>{
+  const number = getRandomInt(243);
+  return fromServer[number];
+});
+
+const reverseList =newArr2.sort((compare, org) => sortFunction(org, compare, 'name'));
+const ol = document.createElement('ol');
+ol.className = 'flex-inner';
+$('form').prepend(ol);
+//const flippedArray = reverseList.reverse();
+
+reverseList.forEach(el =>{
+  const li = document.createElement('li');
+  $(li).append(`<input type="checkbox" value=${el.code} id=${el.code} />`);
+  $(li).append(`<label for=${el.code}>${el.name} </label>`);
+  ol.appendChild(li);
+
+});
+
+//console.log('fromServer', fromServer);
+    }) 
     .catch((err) => console.log(err));
 });
