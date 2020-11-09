4403dfc726a9afecd15a758179d16ed73e117ef8
Kofsterx22 <nuku.monu98@gmail.com>
2020-11-08 20:39:32 -0500

    Update script.js

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -2,17 +2,22 @@ function convertRestaurantsToCategories(restaurantList) {
 const cat = [];
 const res = {};
 
-for (let i = 0; i < res.length; index+= 1) {
-if(res(cat[i])){
+for (let i = 0; i < res.length; i+= 1) {
+cat.push(res[i].cat)
+}
+
+for (let i = 0; i < cat.length; i+= 1) {
+if(res[cat[i]]){
   res[cat[i]] = 0;
 }
 res[cat[i]] +=1;
 }
+const list = Object.keys(res).map((cat),({
 
-const list = Object.keys(res).map((cat,{
   x: res[cat],
   y: cat
 }));
+
 return list;
 }
 
@@ -62,6 +67,48 @@ function runThisWithResultsFromServer(jsonFromServer) {
 
 // Leave lines 52-67 alone; do your work in the functions above
 document.body.addEventListener('submit', async (e) => {
+   const chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	
+	title:{
+		text:"Fortune 500 Companies by Country"
+	},
+	axisX:{
+		interval: 1
+	},
+	axisY2:{
+		interlacedColor: "rgba(1,77,101,.2)",
+		gridColor: "rgba(1,77,101,.1)",
+		title: "Number of Companies"
+	},
+	data: [{
+		type: "bar",
+		name: "companies",
+		axisYType: "secondary",
+		color: "#014D65",
+		dataPoints: [
+			{ y: 3, label: "Sweden" },
+			{ y: 7, label: "Taiwan" },
+			{ y: 5, label: "Russia" },
+			{ y: 9, label: "Spain" },
+			{ y: 7, label: "Brazil" },
+			{ y: 7, label: "India" },
+			{ y: 9, label: "Italy" },
+			{ y: 8, label: "Australia" },
+			{ y: 11, label: "Canada" },
+			{ y: 15, label: "South Korea" },
+			{ y: 12, label: "Netherlands" },
+			{ y: 15, label: "Switzerland" },
+			{ y: 25, label: "Britain" },
+			{ y: 28, label: "Germany" },
+			{ y: 29, label: "France" },
+			{ y: 52, label: "Japan" },
+			{ y: 103, label: "China" },
+			{ y: 134, label: "US" }
+		]
+	}]
+});
+chart.render();
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray();
   fetch('/api', {
